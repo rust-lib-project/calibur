b8e4b5e3ac0d2652cedfe24ebadf6398f99bd221
Little-Wallace <bupt2013211450@gmail.com>
2022-03-09 18:03:09 +0800

    fix test
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- tests/benches/bench_memtable.rs
+++ tests/benches/bench_memtable.rs
@@ -3,7 +3,7 @@ use rand::{thread_rng, Rng, RngCore};
 use rocksdb_rs::{
     InlineSkipListMemtableRep, InternalKeyComparator, MemtableRep, SkipListMemtableRep,
 };
-use std::sync::atomic::AtomicU64;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 use tokio::time::Instant;
 
@@ -12,7 +12,7 @@ fn bench_skiplist<M: MemtableRep + 'static>(
     memtable: Arc<M>,
     max_write_buffer_size: usize,
     max_key_count: u64,
-) {
+) -> usize {
     let now = Instant::now();
     let mut pools = vec![];
     let global_sequence = Arc::new(AtomicU64::new(0));
@@ -24,8 +24,9 @@ fn bench_skiplist<M: MemtableRep + 'static>(
             let mut v: [u8; 256] = [0u8; 256];
             rng.fill(&mut v);
             let mut splice = M::Splice::default();
+            let mut data_size = 0;
             for _ in 0..10000 {
-                let last_sequence = sequence.fetch_add(100, std::sync::atomic::Ordering::SeqCst);
+                let last_sequence = sequence.fetch_add(100, Ordering::SeqCst);
                 if m.mem_size() + 1000 > max_write_buffer_size || last_sequence > max_key_count {
                     break;
                 }
@@ -33,10 +34,12 @@ fn bench_skiplist<M: MemtableRep + 'static>(
                 let gap = rng.next_u32() % 10000;
                 let mut key = b"t_00000001_".to_vec();
                 let l = key.len();
+
                 for i in 0..100 {
                     let j = k + i * gap as u64;
                     key.resize(l, 0);
                     key.extend_from_slice(&j.to_le_bytes());
+                    data_size += key.len() + 8 + i as usize + 20;
                     m.add(
                         &mut splice,
                         &key,
@@ -48,21 +51,23 @@ fn bench_skiplist<M: MemtableRep + 'static>(
                     }
                 }
             }
+            data_size
         });
         pools.push(handle);
     }
 
+    let mut data_size = 0;
     for h in pools {
-        h.join().unwrap();
+        data_size += h.join().unwrap();
     }
 
     let cost = now.elapsed();
     println!(
-        "memtable size: {}, write {} keys cost time: {:?}",
-        memtable.mem_size(),
-        global_sequence.load(std::sync::atomic::Ordering::Acquire),
+        "write {} keys cost time: {:?}",
+        global_sequence.load(Ordering::Acquire),
         cost
     );
+    data_size
 }
 
 fn bench_memtable(c: &mut Criterion) {
@@ -71,17 +76,24 @@ fn bench_memtable(c: &mut Criterion) {
     let max_key_count = 1 * 1024 * 1024;
     {
         let now = Instant::now();
-        let memtable = SkipListMemtableRep::new(comparator.clone(), write_buffer_size);
-        bench_skiplist(c, Arc::new(memtable), write_buffer_size, max_key_count);
+        let memtable = Arc::new(SkipListMemtableRep::new(
+            comparator.clone(),
+            write_buffer_size,
+        ));
+        let data_size = bench_skiplist(c, memtable.clone(), write_buffer_size, max_key_count);
         let cost = now.elapsed();
-        println!("cost time: {:?}", cost);
+        println!(
+            "mem_size: {}, cost time: {:?}",
+            data_size + memtable.mem_size(),
+            cost
+        );
     }
     {
         let now = Instant::now();
-        let memtable = InlineSkipListMemtableRep::new(comparator);
-        bench_skiplist(c, Arc::new(memtable), write_buffer_size, max_key_count);
+        let memtable = Arc::new(InlineSkipListMemtableRep::new(comparator));
+        bench_skiplist(c, memtable.clone(), write_buffer_size, max_key_count);
         let cost = now.elapsed();
-        println!("cost time: {:?}", cost);
+        println!("mem_size: {} cost time: {:?}", memtable.mem_size(), cost);
     }
 }
 
